3553b996aacd524920b22f1bd78982e639b87628
mdrohan <mdsajed210@gmail.com>
2023-04-16 00:37:41 +0600

    complete banglar t-shirt

---
+++ test.js
@@ -0,0 +1,13 @@
+// const data = [
+//   { name: "hero", id: 1221 },
+//   { name: "Karim", id: 1222 },
+//   { name: "Rahim", id: 1223 },
+// ];
+// const result = data.find((person) => person.id === "1221");
+// console.log(result);
+
+const admin = false;
+
+const hello = admin ? <h1>Admin</h1> : "hey";
+
+console.log(hello);
